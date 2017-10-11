6537327c62960354416bc69817a306d27a4ec6ed
dnorwood22 <dnorwood22@gmail.com>
2017-10-11 17:00:01 -0400

    Initial commit

---
+++ dnorwoodBugTracker/Controllers/ProjectsController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using dnorwoodBugTracker.Models;
+using dnorwoodBugTracker.Models.CodeFirst;
+using Microsoft.AspNet.Identity;
+using dnorwoodBugTracker.Models.Helper;
+
+namespace dnorwoodBugTracker.Controllers
+{
+    [Authorize]
+    public class ProjectsController : Universal
+    {
+        // GET: Projects
+        public ActionResult Index()
+        {
+            List<Project> projects = new List<Project>();
+            if(User.IsInRole("Admin") || User.IsInRole("Project Manager"))
+                projects = db.Projects.ToList();
+            else if (User.IsInRole("Developer") || User.IsInRole("Submitter"))
+            {
+                ApplicationUser user = db.Users.Find(User.Identity.GetUserId());
+                projects = user.Projects.ToList();
+            }
+
+            return View(projects);
+        }
+    
+
+        // GET: Projects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+
+        // GET: Projects/Create
+        [Authorize(Roles = "Admin,Project Manger")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Projects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Created,Updated,Title,Description,AuthorId")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.Find(User.Identity.GetUserId());
+                project.AuthorId = user.Id;
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(project);
+        }
+
+        // GET: Projects/Edit/5
+        [Authorize(Roles = "Admin,Project Manager")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        //GET: Projects/Assgin
+        [Authorize (Roles = "Admin, Project Manager")]
+        public ActionResult ProjectAssign(int id, string ticket )
+        {
+            var project = db.Projects.Find(id);
+            var helper = new ProjectAssignHelper();
+            var model = new ProjectsViewModel();
+            model.AssignProject = project;
+            model.AssignProjectId = id;
+            model.SelectedUsers = helper.ListUsersOnProject(id).Select(p => p.Id).ToArray();
+            model.Users = new MultiSelectList(db.Users.ToList(), "Id", "FullName", model.SelectedUsers);
+
+            return View(model);
+        }
+
+        //POST: Projects/Assign
+        [HttpPost]
+        [Authorize(Roles = "Admin, Project Manager")]
+        public ActionResult ProjectAssign(ProjectsViewModel model)
+        {
+            ProjectAssignHelper helper = new ProjectAssignHelper();
+            foreach (var userId in db.Users.Select(r => r.Id).ToList())
+            {
+                helper.RemoveUserFromProject(userId, model.AssignProjectId);
+            }
+            foreach (var userId in model.SelectedUsers)
+            {
+                helper.AddUserToProject(userId, model.AssignProjectId);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //[Authorize(Roles = "Admin, ProjectManager, Developer, Submitter")]
+        //public ActionResult ProjectAssignedTo(int id)
+        //{
+        //    var project = db.Users.Find(id);
+        //    return db.Projects.ToList();
+        //}
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Created,Updated,Title,Description,AuthorId")] Project project)
+        {
+                if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(project);
+
+        }
+
+
+        // GET: Projects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ dnorwoodBugTracker/Controllers/TicketsController.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using dnorwoodBugTracker.Models;
+using dnorwoodBugTracker.Models.CodeFirst;
+using Microsoft.AspNet.Identity;
+using System.IO;
+using dnorwoodBugTracker.Models.Helper;
+
+namespace dnorwoodBugTracker.Controllers
+{
+    [Authorize]
+    public class TicketsController : Universal
+    {
+
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            var tickets = db.Tickets.Include(t => t.AssignToUser).Include(t => t.OwnerUser).Include(t => t.Project).Include(t => t.TicketPriority).Include(t => t.TicketStatus).Include(t => t.TicketType);
+            List<Ticket> Tickets = new List<Ticket>();
+
+            if (User.IsInRole("Admin"))
+            {
+                return View(tickets.ToList());
+            }
+            else if (User.IsInRole("Project Manager"))
+            {
+                return View(tickets.Where(c => c.Project.Users.Any(u => u.Id == user.Id)));
+            }
+            else if (User.IsInRole("Developer"))
+            {
+                return View(tickets.Where(c => c.AssignToUserId == user.Id).ToList());
+            }
+            else if (User.IsInRole("Submitter"))
+            {
+                return View(tickets.Where(c => c.OwnerUserId == user.Id).ToList());
+            }
+            return View(tickets);
+        }
+
+        // GET: Tickets/Details/5
+        public ActionResult Details(int? id)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Role Checking Security
+            if (User.IsInRole("Admin"))
+            {
+                return View(ticket);
+            }
+            else if (User.IsInRole("Project Manager") && !ticket.Project.Users.Any(u => u.Id == user.Id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else if (User.IsInRole("Developer") && ticket.AssignToUserId != user.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else if (User.IsInRole("Submitter") && ticket.OwnerUserId != user.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else if (user.Roles.Count == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Create
+        [Authorize(Roles = "Submitter")]
+        public ActionResult Create()
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title");
+            ViewBag.TicketPriorityId = new SelectList(db.TicketPriorities, "Id", "Name");
+            ViewBag.TicketTypeId = new SelectList(db.TicketTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Submitter")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Description,ProjectId,TicketTypeId,TicketPriorityId")] Ticket ticket)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            TicketHistory ticketHistory = new TicketHistory();
+
+            if (ModelState.IsValid)
+            {
+                ticket.OwnerUserId = user.Id;
+                ticket.Created = DateTimeOffset.UtcNow;
+                ticket.Updated = DateTimeOffset.UtcNow;
+                ticket.TicketStatusId = 1;
+                db.Tickets.Add(ticket);
+
+                ticketHistory.AuthorId = User.Identity.GetUserId();
+                ticketHistory.Created = DateTimeOffset.UtcNow;
+                ticketHistory.TicketId = ticket.Id;
+                ticketHistory.Property = "TICKET CREATED";
+                db.TicketHistories.Add(ticketHistory);
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title", ticket.ProjectId);
+            ViewBag.TicketPriorityId = new SelectList(db.TicketPriorities, "Id", "Name", ticket.TicketPriorityId);
+            ViewBag.TicketTypeId = new SelectList(db.TicketTypes, "Id", "Name", ticket.TicketTypeId);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            Ticket ticket = db.Tickets.Find(id);
+            UserRoleHelper userRoleHelper = new UserRoleHelper();
+
+            var developers = userRoleHelper.UserInRole("Developer");
+            var devsOnProj = developers.Where(d => d.Projects.Any(p => p.Id == ticket.ProjectId));
+
+            ViewBag.AssignToUserId = new SelectList(devsOnProj, "Id", "FullName", ticket.AssignToUserId);
+            ViewBag.OwnerUserId = new SelectList(userRoleHelper.UserInRole("Submitter"), "Id", "FullName", ticket.OwnerUserId); // Set the list to only those in Submitter Role.
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title", ticket.ProjectId);
+            ViewBag.TicketPriorityId = new SelectList(db.TicketPriorities, "Id", "Name", ticket.TicketPriorityId);
+            ViewBag.TicketStatusId = new SelectList(db.TicketStatuses, "Id", "Name", ticket.TicketStatusId);
+            ViewBag.TicketTypeId = new SelectList(db.TicketTypes, "Id", "Name", ticket.TicketTypeId);
+            ViewBag.UserTimeZone = db.Users.Find(User.Identity.GetUserId()).TimeZone;
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+
+            
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Description,Created,Updated,ProjectId,TicketTypeId,TicketPriorityId,TicketStatusId,OwnerUserId,AssignToUserId")] Ticket ticket)
+        {
+
+            if (ModelState.IsValid)
+            { 
+                db.Entry(ticket).State = EntityState.Modified;
+                ticket.Updated = DateTimeOffset.UtcNow;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title", ticket.ProjectId);
+            ViewBag.TicketPriorityId = new SelectList(db.TicketPriorities, "Id", "Name", ticket.TicketPriorityId);
+            ViewBag.TicketStatusId = new SelectList(db.TicketStatuses, "Id", "Name", ticket.TicketStatusId);
+            ViewBag.TicketTypeId = new SelectList(db.TicketTypes, "Id", "Name", ticket.TicketTypeId);
+
+
+            return View(ticket);
+        }
+
+        //POST: TicketAttachments/Create
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddAttachments(IEnumerable<HttpPostedFileBase> files, int ticketId)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            Ticket ticket = db.Tickets.Find(ticketId);
+
+            if (User.IsInRole("Admin") || (User.IsInRole("Project Manager") && ticket.Project.Users.Any(u => u.Id == user.Id)) || (User.IsInRole("Developer") && ticket.AssignToUserId == user.Id) || (User.IsInRole("Submitter") && ticket.OwnerUserId == user.Id))
+            { 
+                foreach (var file in files)
+                {
+                    TicketAttachment attachment = new TicketAttachment();
+                    TicketHistory ticketHistory = new TicketHistory();
+
+                    file.SaveAs(Path.Combine(Server.MapPath("~/TicketAttachments/"), Path.GetFileName(file.FileName)));
+                    attachment.FileUrl = file.FileName;
+
+                    attachment.AuthorId = User.Identity.GetUserId();
+                    attachment.TicketId = ticketId;
+                    attachment.Created = DateTimeOffset.UtcNow;
+                    db.TicketAttachments.Add(attachment);
+
+                    ticketHistory.AuthorId = User.Identity.GetUserId();
+                    ticketHistory.Created = DateTimeOffset.UtcNow;
+                    ticketHistory.TicketId = ticket.Id;
+                    ticketHistory.NewValue = attachment.FileUrl;
+                    ticketHistory.Property = "NEW TICKET ATTACHMENT";
+                    db.TicketHistories.Add(ticketHistory);
+
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Details", "Tickets", new { id = ticketId });
+        }
+
+        //POST: TicketComments/Create
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddComments([Bind(Include = "Id,TicketId,Body")]TicketComment ticketComment)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            var ticket = db.Tickets.Find(ticketComment.TicketId);
+            TicketHistory ticketHistory = new TicketHistory();
+
+            if (ModelState.IsValid && (User.IsInRole("Admin") || (User.IsInRole("Project Manager") && ticket.Project.Users.Any(u => u.Id == user.Id)) || (User.IsInRole("Developer") && ticket.AssignToUserId == user.Id) || (User.IsInRole("Submitter") && ticket.OwnerUserId == user.Id)))
+            {
+
+                ticketComment.AuthorId = User.Identity.GetUserId();
+                ticketComment.TicketId = ticket.Id;
+                ticketComment.Created = DateTimeOffset.UtcNow;
+                db.TicketComments.Add(ticketComment);
+
+                ticketHistory.AuthorId = User.Identity.GetUserId();
+                ticketHistory.Created = ticket.Created;
+                ticketHistory.TicketId = ticket.Id;
+                ticketHistory.NewValue = ticketComment.Body;
+                ticketHistory.Property = "NEW TICKET COMMENT";
+                db.TicketHistories.Add(ticketHistory);
+                db.SaveChanges();
+
+                return RedirectToAction("Details", "Tickets", new { id = ticketComment.TicketId });
+            }
+
+            return RedirectToAction("Index", "Tickets");
+        }
+
+
+        // GET: Tickets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        //POST: Tickets/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ticket ticket = db.Tickets.Find(id);
+            TicketHistory ticketHistory = new TicketHistory();
+
+            ticketHistory.AuthorId = User.Identity.GetUserId();
+            ticketHistory.Created = ticket.Created;
+            ticketHistory.TicketId = ticket.Id;
+            ticketHistory.Property = "OBJECT REMOVED";
+            db.TicketHistories.Add(ticketHistory);
+            db.Tickets.Remove(ticket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+       
+    }
+}
